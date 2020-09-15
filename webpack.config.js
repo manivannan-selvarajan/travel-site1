401fe3f1c9ca0266cebba97b69e811a8f211ef31
manivannan-selvarajan <ms.manivannan1963@gmail.com>
2020-09-15 18:22:55 +0530

    not completed build-process added.

---
+++ webpack.config.js
@@ -0,0 +1,64 @@
+const currentTask = process.env.npm_lifecycle_event
+const path = require('path')
+const {CleanWebpackPlugin} = require('clean-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+const postCSSPlugins = [
+  require('postcss-import'),
+  require('postcss-mixins'),
+  require('postcss-simple-vars'),
+  require('postcss-nested'),
+  require('postcss-hexrgba'),
+  require('autoprefixer')
+]
+
+let cssConfig = {
+  test: /\.css$/i,
+  use: ['css-loader',  {loader: 'postcss-loader', options: {plugins: postCSSPlugins}}], 
+  }
+
+let config = {
+  entry: './app/assets/scripts/App.js',
+  plugins: [new HtmlWebpackPlugin({filename: 'index.html', template: './app/index.html'})],
+  module: {
+    rules: [
+      cssConfig
+    ]
+  }
+}
+
+if (currentTask == 'dev') {
+  cssConfig.use.unshift('style-loader')
+  config.output = {
+    filename: 'bundled.js',
+    path: path.resolve(__dirname, 'app')
+  }
+  config.devServer = {
+    before: function(app, server) {
+      server._watch('./app/**/*.html')
+    },
+    contentBase: path.join(__dirname, 'app'),
+    hot: true,
+    port: 3000,
+    host: '0.0.0.0'
+  }
+  config.mode = 'development'
+}
+
+if (currentTask == 'build') {
+  cssConfig.use.unshift(MiniCssExtractPlugin.loader)
+  postCSSPlugins.push(require('cssnano'))
+  config.output = {
+    filename: '[name].[chunkhash].js',
+    chunkFilename: '[name].[chunkhash].js',
+    path: path.resolve(__dirname, 'dist')
+  }
+  config.mode = 'production'
+  config.optimization = {
+    splitChunks: {chunks: 'all'}
+  }
+  config.plugins.push(new CleanWebpackPlugin(),   new MiniCssExtractPlugin({filename: 'styles.[chunkhash].css'}))
+}
+
+module.exports = config 
